6d373f50e0ef370562a831f87ad0bad16a46f503
Alfredo Jose <alfredojose1022@gmail.com>
2024-02-29 14:47:48 -0300

    feat: add boardlist and preloadings

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -6,6 +6,7 @@ import { EmptyFavorites } from "./empty-favorites";
 import { EmptySearch } from "./empty-search";
 import { api } from "@/convex/_generated/api";
 import { BoardCard } from "./board-card";
+import { NewBoardButton } from "./new-board-button";
 
 interface BoardListProps {
   orgId: string;
@@ -21,7 +22,21 @@ export const BoardList = ({ orgId, query }: BoardListProps) => {
   });
 
   if (data === undefined) {
-    return <div>Loading...</div>;
+    return (
+      <div>
+        <h2 className="text-3xl">
+          {" "}
+          {query.favorites ? "Favorites boards" : "Team boards"}
+        </h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+          <NewBoardButton orgId={orgId} disabled />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+        </div>
+      </div>
+    );
   }
 
   if (!data.length && query.search) {
@@ -42,6 +57,7 @@ export const BoardList = ({ orgId, query }: BoardListProps) => {
         {query.favorites ? "Favorites boards" : "Team boards"}
       </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+        <NewBoardButton orgId={orgId} />
         {data.map((board) => (
           <BoardCard
             key={board._id}
---
+++ app/(dashboard)/_components/new-board-button.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { api } from "@/convex/_generated/api";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { cn } from "@/lib/utils";
+import { Plus } from "lucide-react";
+import { toast } from "sonner";
+
+interface NewBoardButtonProps {
+  orgId: string;
+  disabled?: boolean;
+}
+
+export const NewBoardButton = ({ orgId, disabled }: NewBoardButtonProps) => {
+  const { mutate, pending } = useApiMutation(api.board.create);
+
+  const onClick = () => {
+    if (!orgId) return null;
+
+    mutate({
+      tile: "untitled",
+      orgId: orgId,
+    })
+      .then((id) => {
+        toast.success("Board created");
+        // redirect TO BOARD
+      })
+      .catch((error) => {
+        toast.error("Falied to create baord");
+      });
+  };
+
+  return (
+    <button
+      disabled={pending || disabled}
+      className={cn(
+        "col-span-1 aspect-[100/127] bg-blue-600 rounded-lg hover:bg-blue-800 flex flex-col items-center justify-center py-6",
+        (pending || disabled) &&
+          "opacity-75 hover:bg-blue-600 cursor-not-allowed"
+      )}
+      onClick={() => onClick()}
+    >
+      <div />
+      <Plus className="h-12 w-12 text-white stroke-1" />
+      <p className="text-white font-light text-sm">New baord</p>
+    </button>
+  );
+};
