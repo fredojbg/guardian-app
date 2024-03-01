7900475c7284ffbff83cd62d1a0f090de14a3e86
Alfredo Jose <alfredojose1022@gmail.com>
2024-03-01 00:05:53 -0300

    fix: add redirect

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -7,9 +7,11 @@ import { useApiMutation } from "@/hooks/use-api-mutation";
 import { useOrganization } from "@clerk/nextjs";
 
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
 export const EmptyBoards = () => {
+  const router = useRouter();
   const { organization } = useOrganization();
   const { mutate, pending } = useApiMutation(api.board.create);
 
@@ -22,7 +24,7 @@ export const EmptyBoards = () => {
     })
       .then((id) => {
         toast.success("Board created");
-        // redirect TO BOARD
+        router.push(`/board/${id}`);
       })
       .catch((error) => {
         toast.error("Falied to create baord");
